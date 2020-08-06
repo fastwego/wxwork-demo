0657cadac6a07103c00a1c0480649fe0c702504e
Ceelog <chenwei@ceelog.org>
2020-08-06 20:34:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+## wechat4work-demo 
+
+A demp project use fastwego/wechat4work sdk
+
+- checkout project `git clone https://github.com/fastwego/wechat4work-demo.git`
+- install fastwego/wechat4work `go get -u github.com/fastwego/wechat4work`
+- build `go build`
+- edit config in `.env.dist` file and rename to `.env`
+- run `wechat4work-demo` & view `http://localhost/api/weixin/contact`
+- that's all & good luck ;)
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/fastwego/wechat4work/corporation"
+
+	"github.com/fastwego/offiaccount/type/type_message"
+
+	"github.com/spf13/viper"
+
+	"github.com/gin-gonic/gin"
+)
+
+var Corp *corporation.Corporation
+var ContactApp *corporation.App
+
+func init() {
+	// 加载配置文件
+	viper.SetConfigFile(".env")
+	_ = viper.ReadInConfig()
+
+	Corp = corporation.New(corporation.CorporationConfig{Corpid: viper.GetString("CROPID")})
+	ContactApp = Corp.NewApp(corporation.AppConfig{
+		AgentId:        viper.GetString("AGENTID"),
+		Secret:         viper.GetString("SECRET"),
+		Token:          viper.GetString("TOKEN"),
+		EncodingAESKey: viper.GetString("EncodingAESKey"),
+	})
+}
+
+func HandleMessage(c *gin.Context) {
+
+	body, _ := ioutil.ReadAll(c.Request.Body)
+	log.Println(string(body))
+
+	message, err := ContactApp.Server.ParseXML(body)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var output interface{}
+	switch message.(type) {
+	case type_message.MessageText: // 文本 消息
+		msg := message.(type_message.MessageText)
+
+		// 回复文本消息
+		output = type_message.ReplyMessageText{
+			ReplyMessage: type_message.ReplyMessage{
+				ToUserName:   type_message.CDATA(msg.FromUserName),
+				FromUserName: type_message.CDATA(msg.ToUserName),
+				CreateTime:   strconv.FormatInt(time.Now().Unix(), 10),
+				MsgType:      type_message.ReplyMsgTypeText,
+			},
+			Content: type_message.CDATA(msg.Content),
+		}
+	}
+
+	ContactApp.Server.Response(c.Writer, c.Request, output)
+}
+
+func main() {
+
+	router := gin.New()
+	router.Use(gin.Logger(), gin.Recovery())
+
+	router.GET("/api/weixin/contact", func(c *gin.Context) {
+		ContactApp.Server.EchoStr(c.Writer, c.Request)
+	})
+	router.POST("/api/weixin/contact", HandleMessage)
+
+	svr := &http.Server{
+		Addr:    viper.GetString("LISTEN"),
+		Handler: router,
+	}
+
+	go func() {
+		err := svr.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalln(err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	timeout := time.Duration(5) * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	if err := svr.Shutdown(ctx); err != nil {
+		log.Fatalln(err)
+	}
+}
